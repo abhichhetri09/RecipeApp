6522dd6ac3881b4dec7518240ce1266bc57204bc
Abhishek chhetri <abhichhetri@outlook.com>
2023-11-07 15:18:59 +0200

    Nav bar added to the main page using bootstrap

--- src/main/java/fi/haagahelia/recipeapp/web/RecipeController.java
+++ src/main/java/fi/haagahelia/recipeapp/web/RecipeController.java
@@ -2,11 +2,9 @@ package fi.haagahelia.recipeapp.web;
 
 import fi.haagahelia.recipeapp.domain.Recipe;
 import fi.haagahelia.recipeapp.domain.RecipeRepository;
-import fi.haagahelia.recipeapp.domain.User;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import ch.qos.logback.classic.Logger;
 
